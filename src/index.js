727d26a3da6c00e74f0a4924bcc46bd4cfc6e2a6
butevichanastas <118684036+butevichanastas@users.noreply.github.com>
2023-01-24 14:57:33 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 module.exports = function reverse(n) {
+	
 	if (n < 0) {
 		let positiveNumber = Math.abs(n);
-		let str = positiveNumber.toString().split('').reverse().join('');
+		let str = positiveNumber.toString().split('').reverse('').join('').toNumber();
 		console.log(str)}
 	else {
-		let str = n.toString().split('').reverse().join('');
+		let str = n.toString().split('').reverse('').join('').toNumber();
 		console.log(str);
 		}
 }
